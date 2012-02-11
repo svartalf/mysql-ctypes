97942b16930d1ee4174f57af95748aacefe0e624
SvartalF <self@svartalf.info>
2012-02-11 10:18:06 +0900

    PyPy classifier added to setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Database",
     ],
 )
